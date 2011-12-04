03591a3e7f3a242cd297653cdfec4532aa4bd3bc
unknown <kp.ruby.dev@(none)>
2011-12-04 23:06:57 +0200

    sīki labojumi 354

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-#require 'rails_sql_views'
 
 # Initialize the rails application
 Billing::Application.initialize!
